b8d55a9eb26d9a5d6670cd33cc8f21097ad852ff
ahlam10 <ahlam_11a@hotmail.com>
2023-11-26 16:13:30 +0300

    Initial Commit

---
+++ aaaaa/aaaaaApp.swift
@@ -0,0 +1,17 @@
+//
+//  aaaaaApp.swift
+//  aaaaa
+//
+//  Created by Ahlam ALshehri on 12/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct aaaaaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
